6d08d861cae07af56c6aae8663f22672f084d92b
kneumann2023 <karolina.chmelikova@gmail.com>
2024-04-03 07:58:33 +0200

    up

--- PY2024_lekce04_GitHub/01_cviceni.py
+++ PY2024_lekce04_GitHub/01_cviceni.py
@@ -17,7 +17,7 @@ books = [
 ]
 
 
-# pocet knih
+# pocet prectenych knih
 # var 1
 print(len(books))
 
@@ -26,7 +26,7 @@ num_books = 0
 for book in books:
     num_books += 1
 
-# ad A 
+# ad A - celkovy pocet prectenych hstran
 num_pages = 0
 for book in books:
     num_pages += book["pages"]
@@ -38,21 +38,24 @@ print(num_pages / num_books)
 # ad A - bonus spocti ktera kniha ma nejvic stran
 # pomoci cyklu - nejdriv definovat zacatek a pak porovnavat s kazdym dalsim zaznamem 
 
-max_num_pages = 0
-for book in books:
-   if book["pages"] > max_num_pages:
-       max_num_pages = book["pages"]
+max_num_pages = 0                            # definovat začátek - maximum = 0
+for book in books:                
+   if book["pages"] > max_num_pages:         # je-li pocet stran v dane knizce vyšší, než max
+       max_num_pages = book["pages"]         # pak je tato kniha maximum
 print(max_num_pages)
 
-# ad A - bonus spoti ktera kniha ma nejmin stran
-# pomoci cyklu - zde musim zacatek dat vysoky, aby davalo smysl porovnavani
-min_num_pages = 100000
-min_num_pages = books[0]["pages"]
+# ad A - bonus spocti, ktera kniha ma nejmin stran
+
+min_num_pages = 100000                       # záměrně vysoké, aby porovnávání dávalo smysl
+min_num_pages = books[0]["pages"]            # NEBO uložit počet stran 1. knihy a s ní porovnávat
 for book in books:
    if book["pages"] < min_num_pages:
        min_num_pages = book["pages"]
 print(min_num_pages)
 
 # ad B
-num_good_books = 0
-#DOPLNIT
+pocet_knih_s_vysokym_hodnocenim = 0
+for book in books:
+    if book["rating"] >= 8:
+        pocet_knih_s_vysokym_hodnocenim += 1
+print("Počet knih s hodnocením alespoň 8:", pocet_knih_s_vysokym_hodnocenim)
