03ad3995cf0fcf7d35c93a4bd26d3ac913aa2650
kneumann2023 <karolina.chmelikova@gmail.com>
2024-04-26 16:22:03 +0200

    up

--- PY2024_ukol1/ukol_dataclass.py
+++ PY2024_ukol1/ukol_dataclass.py
@@ -14,14 +14,17 @@ class Item:
 class Pizza(Item):
     ingredients: dict   # ingredience: vaha (g)
 
-    def add_extra(self, ingredient, quantity, price_per_ingredient):
-        if ingredient > 0: 
+def add_extra(self, ingredient, quantity, price_per_ingredient):
+        if self.ingredients[ingredient] > 0: 
             total_pizza_price = super().price + (quantity * price_per_ingredient)
             return total_pizza_price
         else: 
             total_pizza_price = super().price
             return total_pizza_price
-
+    
     def __str__(self):
         return f"Pizza {self.name} s extra ingrediencemi ({ingredient}) stojí {total_pizza_price} Kč." 
     
+margarita = Pizza("Margarita", 200, {"sýr": 100, "rajčata": 150})
+margarita.add_extra("olivy", 50, 10)
+
