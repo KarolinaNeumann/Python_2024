c666e017919a2a22278ea4d3041559e5c14ff14e
kneumann2023 <karolina.chmelikova@gmail.com>
2024-04-27 00:13:17 +0200

    up

--- PY2024_ukol1/ukol_dataclass.py
+++ PY2024_ukol1/ukol_dataclass.py
@@ -1,6 +1,7 @@
 # Rozvoz pizzy
 
-from dataclasses import dataclass      
+from dataclasses import dataclass, field  
+from typing import Dict   
 
 @dataclass  
 class Item:
@@ -8,23 +9,32 @@ class Item:
     price: float   
     
     def __str__(self):
-        return f"{self.name}: {self.price} Kč."
+        return f"{self.name}: {self.price} Kč"
 
 @dataclass
 class Pizza(Item):
-    ingredients: dict   # ingredience: vaha (g)
+    ingredients: Dict[str, int] = field(default_factory=dict)  # slovník = {ingredience: vaha}
 
     def add_extra(self, ingredient, quantity, price_per_ingredient):
-        if self.ingredients[ingredient] > 0: 
+        self.ingredients[ingredient] = quantity
+        if quantity > 0:
             total_pizza_price = super().price + (quantity * price_per_ingredient)
             return total_pizza_price
         else: 
             total_pizza_price = super().price
             return total_pizza_price
-    
+        
     def __str__(self):
-        return f"Pizza {self.name} s extra ingrediencemi ({ingredient}) stojí {total_pizza_price} Kč." 
-    
-margarita = Pizza("Margarita", 200, {"sýr": 100, "rajčata": 150})
-margarita.add_extra("olivy", 50, 10)
+        return f"Pizza {self.name} s extra ingrediencemi:{self.ingredient} stojí {total_pizza_price} Kč."
 
+margarita = Pizza("Margarita", 200, {"sýr": 100, "rajčata": 150})
+margarita.add_extra("olivy", 50, 10)       
+print(margarita)
+
+
+@dataclass
+class Drink(Item):
+    volume: int    # objem napoje (ml)
+
+    def __str__(self):
+        return f"Nápoj " + super().__str__() + f" (objem nápoje je {self.volume} ml)"
