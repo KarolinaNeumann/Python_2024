b607bdbb1b5385917a91c80fcb6c5eea3ad09bc9
kneumann2023 <karolina.chmelikova@gmail.com>
2024-04-26 16:18:45 +0200

    up

---
+++ PY2024_ukol1/ukol_dataclass.py
@@ -0,0 +1,27 @@
+# Rozvoz pizzy
+
+from dataclasses import dataclass      
+
+@dataclass  
+class Item:
+    name:  str      
+    price: float   
+    
+    def __str__(self):
+        return f"{self.name}: {self.price} Kč."
+
+@dataclass
+class Pizza(Item):
+    ingredients: dict   # ingredience: vaha (g)
+
+    def add_extra(self, ingredient, quantity, price_per_ingredient):
+        if ingredient > 0: 
+            total_pizza_price = super().price + (quantity * price_per_ingredient)
+            return total_pizza_price
+        else: 
+            total_pizza_price = super().price
+            return total_pizza_price
+
+    def __str__(self):
+        return f"Pizza {self.name} s extra ingrediencemi ({ingredient}) stojí {total_pizza_price} Kč." 
+    
